ffd967f796f46c055c38fa18ca22beb9bbf05e2e
superekjan <superekjan@users.noreply.github.com>
2026-05-27 10:43:21 +0800

    feat: 光猫位置向右移动2格
    - 光猫位置从[10, 0.5, 3.75]移动到[12, 0.5, 3.75]

--- floorplan.js
+++ floorplan.js
@@ -382,7 +382,7 @@ class FloorPlan3D {
             { pos: [-10, 0.5, -3.75], color: 0x7b2cbf, name: '路由1', signal: 88 },
             { pos: [-10, 0.5, 3.75], color: 0x7b2cbf, name: '路由2', signal: 82 },
             { pos: [0, 0.5, 0], color: 0xff6b6b, name: '电视', signal: 70 },
-            { pos: [10, 0.5, 3.75], color: 0x00d4ff, name: '光猫', signal: 95 }
+            { pos: [12, 0.5, 3.75], color: 0x00d4ff, name: '光猫', signal: 95 }
         ];
 
         deviceData.forEach(device => {
